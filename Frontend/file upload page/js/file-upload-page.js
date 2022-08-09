dcc42905dd001bf3356148c7b78433c4d4dbe8d3
AkanEdwin <edwinakaninyene@gmail.com>
2022-08-09 12:37:04 +0100

    upload page commit

--- Frontend/file upload page/js/file-upload-page.js
+++ Frontend/file upload page/js/file-upload-page.js
@@ -1,34 +1,77 @@
-const initApp = () => {
-	const box = document.querySelector('#box')
+const dragArea = document.querySelector('#box');
+const dragText = document.querySelector('.dragtext');
 
-	const active = ( ) => droparea.classList.add("green-border");
+let dropClick = document.querySelector(".box-content");
+let input = document.querySelector('.drop_input');
 
-	const inactive = () => droparea.classList.remove("green-border");
+let file;
 
-	const prevents = (e) => e.preventDefault();
+dropClick.onclick = () => {
+	input.click();
 
-	['dragenter', 'dragover', 'dragleave', 'drop'].forEach(evtName => 
-		{droparea.addEventListener(evtName, prevents);
-	});
+};
 
-	['dragenter', 'dragover'].forEach(evtName => {
-		droparea.addEventListener(evtName, active);
-	});
+//when browse
+input.addEventListener('change', function () {
+	file = this.files[0];
+	dragArea.classList.add('actives');
+	displayFile();
 
-	['dragleave', 'drop'].forEach(evtName => {
-		droparea.addEventListener(evtName, inactive);
-	});
 
-	droparea.addEventListener("drop, handleDrop");
+});
 
-}
 
-document.addEventListener("DOMContentLoaded, initApp");
+//when file is in the drag area
+dragArea.addEventListener('dragover', (event) => {
+	event.preventDefault();
+	dragText.textContent = 'Release to upload';
+	dragArea.classList.add('actives');
+	//console.log("file is inside the drag area");
+})
 
-const handleDrop = (e) => {
-	const dt = e.dataTransfer;
-	const files = dt.files;
-	const fileArray = [...files];
-	console.log(files);
-	console.log(FilesArray);
+//when file leaves the drag area
+dragArea.addEventListener('dragleave', (event) => {
+	event.preventDefault();
+	dragText.textContent = 'Drag and drop a file here';
+	dragArea.classList.remove('actives');
+	//console.log('file left the drag area');
+})
+
+//when the file is dropped
+
+dragArea.addEventListener('drop', (event) => {
+	event.preventDefault();
+	
+	file = event.dataTransfer.files[0];
+	//console.log(file);
+	displayFile();
+
+		
+});
+
+function displayFile() {
+	let fileType = file.type;
+
+	//console.log('file has been dropped');
+	let validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'image/svg', 'image/svg+xml', 'application/pdf', 'application/json', 'text/csv', 'video/mp4',];
+
+	if(validExtensions.includes(fileType)) {
+		let fileReader = new FileReader();
+	
+
+		fileReader.onload = () => {
+			let fileURL = fileReader.result;
+			console.log(fileURL);
+		};
+		fileReader.readAsDataURL(file);
+	} else {
+		alert('File type not supported');
+		dragArea.classList.remove('active')
+
+	}
+
+	
+	
+	//console.log('file has been dropped');
+
 }
